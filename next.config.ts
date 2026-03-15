b6f94941981ae4dbbf17c129285405613ac28370
aliff2712 <alifmuhamadramdan75@gmail.com>
2026-03-15 13:00:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,17 @@
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*\.supabase\.co\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'supabase-cache',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24, // 1 hari
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'external-cache',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24,
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/static\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-cache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 hari
-        },
-      },
-    },
-  ],
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   images: {
     remotePatterns: [
       {
